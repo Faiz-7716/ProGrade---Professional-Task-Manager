07922b7b9911f9e593514ed946fd7e755cd8a562
Mohammed Faiz <pmdfaiz08@gmail.com>
2025-09-28 15:19:47 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "TODO: Your API key",
-  authDomain: "TODO: Your auth domain",
-  projectId: "TODO: Your project ID",
-  storageBucket: "TODO: Your storage bucket",
-  messagingSenderId: "TODO: Your messaging sender ID",
-  appId: "TODO: Your app ID"
+  projectId: 'studio-2384447676-2ec49',
+  appId: '1:299612523661:web:d3cd9e5e089f219a4b66c4',
+  apiKey: 'AIzaSyD_RS2SjXbD_xY33bD0I0AkJbvv2Ill2D4',
+  authDomain: 'studio-2384447676-2ec49.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '299612523661',
+  storageBucket: 'studio-2384447676-2ec49.appspot.com'
 };
 
 // Initialize Firebase
